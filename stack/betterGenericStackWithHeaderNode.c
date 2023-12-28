6ba148fd8d8d37f733309ae27537e7cba2eafa62
tinku797 <pranjal.sisodia797@gmail.com>
2023-12-28 15:48:01 +0530

    to handle edge cases, created betterGenericStackWithHeaderNode

--- stack/betterGenericStackWithHeaderNode.c
+++ stack/betterGenericStackWithHeaderNode.c
@@ -25,7 +25,17 @@ s->elementSize = elementSize;
 void push(stack*s,void* addrOfDataToCopy,int* success,int* errorno)
 {
 if(success != NULL) *success = 0; //assumed unsuccessful by default.
-if(errorno != NULL) *errorno = 0; //errorno 0 for push(), can be set in doc to - function not fully executed.
+if(errorno != NULL) *errorno = 100; //errorno 100 for push(), can be set in doc to - function not fully executed.
+if(s == NULL)
+{
+if(errorno != NULL) *errorno = 2; //errorno 2 for push(), can be set in doc to - cannot accept NULL as argument for the parameter stack*s.
+return;
+}
+if(addrOfDataToCopy == NULL)
+{
+if(errorno != NULL) *errorno = 3; //errorno 3 for push(), can be set in doc to - cannot accept NULL as argument for the parameter void* addrOfDataToCopy. 
+return;
+}
 node* temp = (node*)malloc(sizeof(node));
 if(temp == NULL)
 {
@@ -43,7 +53,7 @@ temp->next = s->top;
 s->top = temp;
 s->size++;
 if(success != NULL) *success = 1;
-if(errorno != NULL) *errorno = 100;   //errorno 100 for push(), can be set in doc to - no error, operation successful.
+if(errorno != NULL) *errorno = 0;   //errorno 0 for push(), can be set in doc to - no error, operation successful.
 }
 }
 
@@ -53,25 +63,35 @@ if(s->top == NULL) return 1;
 else return 0;
 }
 
-//you have to use memcpy in pop, to return the data. Maybe not.
-void pop(stack*s,void** returnedPtr2data,int* success,int* errorno)  //could even change int* success and int* errorno to unsigned int* success, and unsigned int* errorno
+void pop(stack*s,void* addrOfVarToCopyDataTo,int* success,int* errorno)  //could even change int* success and int* errorno to unsigned int* success, and unsigned int* errorno
 {
 if(success != NULL) *success = 0;  //by default, assumed unsuccessful.
-if(errorno != NULL) *errorno = 0;  //errorno 0 for pop(), could be set in documentation to - full function execution could not be completed.
+if(errorno != NULL) *errorno = 100;  //errorno 100 for pop(), could be set in documentation to - full function execution could not be completed.
+if(s == NULL)
+{
+if(errorno != NULL) *errorno = 2; //errorno 2 for pop(), can be set in doc to - cannot accept NULL as argument for the parameter stack*s.
+return;
+}
+if(addrOfVarToCopyDataTo == NULL)
+{
+if(errorno != NULL) *errorno = 3; //errorno 3 for pop(), can be set in doc to - cannot accept NULL as argument for the parameter void* addrOfDataToCopy. 
+return;
+}
 if(!isEmpty(s))
 {
-void* ptr2data = s->top->ptr2data;
+void* p2d = s->top->ptr2data;
+memcpy(addrOfVarToCopyDataTo,s->top->ptr2data,s->elementSize); //one of the reasons why elementSize is required as a data member for generic stack
 node* temp = s->top;
 s->top = s->top->next;
+free(p2d);
 free(temp);
 s->size--;
-*returnedPtr2data = ptr2data;
 if(success != NULL) *success = 1;
-if(errorno != NULL) *errorno = 100;  //errorno 100 for pop(), could be set in doc to - no error, operation successful.
+if(errorno != NULL) *errorno = 0;  //errorno 0 for pop(), could be set in doc to - no error, operation successful.
 }
 else
 {
-*returnedPtr2data = NULL;
+// *returnedPtr2data = NULL; -- not really required 
 // *success = 0; --not really needed as *success is already set to 0 by default above.
 if(errorno != NULL) *errorno = 1;  //errorno 1 for pop(), could be set to in documentation to - stack is empty, hence cannot pop.
 }
@@ -87,8 +107,11 @@ init(&s1,sizeof(int));
 
 int x = 241;
 int y = 421;
-push(&s1,&x,NULL,NULL);
-push(&s1,&y,NULL,NULL);
+int success, errorno;
+push(&s1,&x,&success,&errorno);
+printf("success: %d, errrorno: %d\n",success,errorno);
+push(&s1,&y,&success,&errorno);
+printf("success: %d, errrorno: %d\n",success,errorno);
 
 /*
 for(int i=0;i<899;i++)
@@ -99,11 +122,10 @@ printf("Popped data from stack: %d\n",*(pop(&s1)));
 
 while(!isEmpty(&s1))
 {
-int* data;
-int success, errorno;
-pop(&s1,&(void*)data,&success,&errorno);
+int data;
+pop(&s1,&data,&success,&errorno);
 printf("success: %d, errrorno: %d\n",success,errorno);
-printf("popped data:%d\n",*(int*)data);
+printf("popped data:%d\n",data);
 }
 
 return 0;
